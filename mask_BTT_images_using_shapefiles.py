dfa6b167c4815d716545c4aed2846a998c4c73c1
annusgit <annusgit@gmail.com>
2020-12-27 23:42:47 +0500

    ready for clipping 5

--- mask_BTT_images_using_shapefiles.py
+++ mask_BTT_images_using_shapefiles.py
@@ -54,7 +54,7 @@ def gdal_warp_masking(district, shapefile_path, image_path, destination_path):
 
 
 if __name__ == "__main__":
-    for year in [2014, 2016, 2017, 2018, 2019, 2020]:
+    for year in [2019, 2020]:
         for district in all_districts:
             shapefile_path = os.path.join(shapefile_masks_path, '{}.shp'.format(district))
             image_path = os.path.join(images_path, 'landsat8_4326_30_{}_region_{}.tif'.format(year, district))
