0c31fd7ac45d2a732e6288bd7406eabc12a710d0
annusgit <annusgit@gmail.com>
2020-12-27 22:59:40 +0500

    ready for clipping

--- mask_BTT_images_using_shapefiles.py
+++ mask_BTT_images_using_shapefiles.py
@@ -5,12 +5,18 @@ import subprocess
 import gdal
 import os
 
-shapefile_masks_path = "E:\\Forest Cover - Redo 2020\\Shapefiles_clipped_to_district_vectors\\"
-raster_masks_path = "E:\\Forest Cover - Redo 2020\\Google Cloud - Training\\Training Data\\District_Shapefiles_as_Clipping_bands\\"
-images_path = "E:\\Forest Cover - Redo 2020\\Google Cloud - Training\\Training Data\\30m_4326_btt_2020_unclipped_images\\"
 all_districts = ["abbottabad", "battagram", "buner", "chitral", "hangu", "haripur", "karak", "kohat", "kohistan", "lower_dir", "malakand", "mansehra",
                  "nowshehra", "shangla", "swat", "tor_ghar", "upper_dir"]
 
+# local PC
+# shapefile_masks_path = "E:\\Forest Cover - Redo 2020\\Shapefiles_clipped_to_district_vectors\\"
+# raster_masks_path = "E:\\Forest Cover - Redo 2020\\Google Cloud - Training\\Training Data\\District_Shapefiles_as_Clipping_bands\\"
+# images_path = "E:\\Forest Cover - Redo 2020\\Google Cloud - Training\\Training Data\\30m_4326_btt_2020_unclipped_images\\"
+
+# remote GCP
+shapefile_masks_path = "/home/azulfiqar_bee15seecs/btt_shapefiles/"
+images_path = "/home/azulfiqar_bee15seecs/btt_shapefiles/BTT_2014_2020_unclipped_images/"
+destination_path = "/home/azulfiqar_bee15seecs/btt_shapefiles/BTT_2014_2020_clipped_images/"
 """
     gdalwarp -of GTiff -tr 0.00026949458523585804 -0.00026949458523585566 -tap -cutline "E:\\Forest Cover - Redo 2020\\Shapefiles_clipped_to_district_vectors\\shangla.shp" -cl shangla -crop_to_cutline "E:\\Forest Cover - Redo 2020\\Google Cloud - Training\\Training Data\\30m_4326_btt_2020_unclipped_images\\landsat8_4326_30_2015_region_shangla.tif" C:/Users/annus/AppData/Local/Temp/processing_DHAjdp/93bb4beb509b4ad99595d252f8886cc2/OUTPUT.tif
 """
@@ -48,14 +54,17 @@ def gdal_warp_masking(district, shapefile_path, image_path, destination_path):
 
 
 if __name__ == "__main__":
-    for district in all_districts:
-        shapefile_path = os.path.join(shapefile_masks_path, f'{district}.shp')
-        image_path = os.path.join(images_path, f'landsat8_4326_30_2015_region_{district}.tif')
-        destination_path = f'clipped_images\\clipped_{district}.tif'
-        gdal_warp_masking(district=district, shapefile_path=shapefile_path, image_path=image_path, destination_path=destination_path)
-        streched_image = get_clipped_image(this_path=f"clipped_images\\clipped_{district}.tif")
-        # show the masked image
-        if streched_image is not None:
-            plt.imshow(streched_image)
-            plt.show()
+    for year in [2014, 2016, 2017, 2018, 2019, 2020]:
+        for district in all_districts:
+            shapefile_path = os.path.join(shapefile_masks_path, f'{district}.shp')
+            image_path = os.path.join(images_path, f'landsat8_4326_30_{year}_region_{district}.tif')
+            destination_path = destination_path, f'clipped_{district}_{year}.tif'
+            gdal_warp_masking(district=district, shapefile_path=shapefile_path, image_path=image_path, destination_path=destination_path)
+            # streched_image = get_clipped_image(this_path=f"clipped_images\\clipped_{district}.tif")
+            # # show the masked image
+            # if streched_image is not None:
+            #     plt.imshow(streched_image)
+            #     plt.show()
+            pass
+        pass
     pass
